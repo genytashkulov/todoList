9eff3b279ba6566110242e6670570e7318807506
Mr Geny <genyjon@gmail.com>
2021-09-17 23:33:44 +0300

    first commit

---
+++ tasks/views.py
@@ -0,0 +1,51 @@
+from django.shortcuts import render,HttpResponse, redirect
+
+from .models import *
+
+from .forms import *
+
+# Create your views here.
+
+#
+def home(request):
+    #create
+    tasks = Task.objects.all()
+
+    form = TaskForm()
+
+#create form that push onto list below
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+
+#
+    context = {'tasks': tasks, 'form': form}
+    return render(request,'list.html',context)
+
+
+def update(request, pk):
+    task = Task.objects.get(id=pk)
+#
+    form = TaskForm(instance=task)
+    if request.method == 'POST':
+        form = TaskForm(request.POST,instance=task)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+
+    context = {'form': form}
+
+    return render(request,'update.html',context)
+
+def delete(request, pk):
+    item = Task.objects.get(id=pk)
+
+    if request.method == 'POST':
+        item.delete()
+        return redirect('/')
+
+
+    context = {'item': item}
+    return render(request, 'delete.html',context)
